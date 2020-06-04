d562d9048e3ae3a9f381afb2a14cd89c04060cbe
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-06-04 22:18:20 +0100

    lonely checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,16 +1,1 @@
-
-
-def has_voted(bot, user_id):
-    has_voted_ = bot.cache.get("votes", user_id)
-    if has_voted_ is not None:
-        if has_voted_['expires'] is not None:
-            return 1
-        else:
-            return 0
-    else:
-        has_voted_ = bot.database.get_vote(user_id)
-        bot.cache.store("votes", user_id, has_voted)
-        if has_voted_['expires'] is not None:
-            return 1
-        else:
-            return 0
+# Its empty too empty :(
