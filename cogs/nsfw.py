41f66a086e33319e14d7878687717588efea9e18
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-06-04 20:59:48 +0100

    nsfw

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -62,8 +62,53 @@ class NSFW(commands.Cog):
             resp = await self.collector.get_from_crunchy(tag=tag)
             embed = discord.Embed(color=self.bot.colour)
             embed.set_image(url=resp['url'])
-            embed.set_footer(text="Images from our API https://crunchy-bot.live/api/endpoints")
+            embed.set_footer(text="https://crunchy-bot.live/api/endpoints")
             await ctx.send(embed=embed)
+
+    @commands.command()
+    async def ass(self, ctx):
+        if not ctx.channel.is_nsfw():
+            return await ctx.send("<:cheeky:717784139226546297> Oops! NSFW commands can only be used in NSFW channels.")
+
+        if ctx.has_voted >= 1:
+            if ctx.guild is not None:
+                if not ctx.guild_config.nsfw_enabled:
+                    return await ctx.send("<:cheeky:717784139226546297> Oops! NSFW is disabled in this server,"
+                                          " ask a admin to run `togglensfw` if this should be enabled.")
+            resp = await self.collector.get_from_neko(type_="ass")
+            await self.send_embed(ctx, resp['url'])
+
+    @commands.command()
+    async def pussy(self, ctx):
+        if not ctx.channel.is_nsfw():
+            return await ctx.send("<:cheeky:717784139226546297> Oops! NSFW commands can only be used in NSFW channels.")
+
+        if ctx.has_voted >= 1:
+            if ctx.guild is not None:
+                if not ctx.guild_config.nsfw_enabled:
+                    return await ctx.send("<:cheeky:717784139226546297> Oops! NSFW is disabled in this server,"
+                                          " ask a admin to run `togglensfw` if this should be enabled.")
+            resp = await self.collector.get_from_neko(type_="pussy")
+            await self.send_embed(ctx, resp['url'])
+
+    @commands.command(name="gonewild")
+    async def gone_wild(self, ctx):
+        if not ctx.channel.is_nsfw():
+            return await ctx.send("<:cheeky:717784139226546297> Oops! NSFW commands can only be used in NSFW channels.")
+
+        if ctx.has_voted >= 1:
+            if ctx.guild is not None:
+                if not ctx.guild_config.nsfw_enabled:
+                    return await ctx.send("<:cheeky:717784139226546297> Oops! NSFW is disabled in this server,"
+                                          " ask a admin to run `togglensfw` if this should be enabled.")
+            resp = await self.collector.get_from_neko(type_="gonewild")
+            await self.send_embed(ctx, resp['url'])
+
+    async def send_embed(self, ctx, url):
+        embed = discord.Embed(color=self.bot.colour)
+        embed.set_image(url=url)
+        embed.set_footer(text="https://nekobot.xyz/api/")
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
