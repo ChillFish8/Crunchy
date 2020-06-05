034b36ee9b895acf025cb6f7063ddc5512eedbf0
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-06-05 11:18:54 +0100

    tasks

---
+++ resources/archieve/anime_examples.py
@@ -0,0 +1,33 @@
+LIST = ['Darling in the franxx',
+        'Fire Force',
+        'GATE',
+        'Domestic Girlfriend',
+        'A Certain Scientific Railgun',
+        'ROOM CAMP',
+        'Re:ZERO',
+        'In/Spectre',
+        'Asterisks War',
+        'Hundred',
+        ]
+
+MANGA = ['Domestic Girlfriend',
+         'Attack on Titan',
+         'EDENS ZERO',
+         'Inside Mari',
+         'The Seven Deadly Sins',
+         ]
+
+GROUPS = ['Manga',
+          'Re:ZERO',
+          'Zero Two Fans',
+          'Rem Best girl']
+
+WATCHLIST = ['In/Spectre',
+             'Darling in the Franxx',
+             'JoJo',
+             'Re:Zero',
+             'Fire Force',
+             'Ascendance of a bookworm',
+             'Radiant',
+             'Ahiru no sora',
+             'Chihayafuru ']
