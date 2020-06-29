b769d37bb826ab68df33e698b45c97ff3e537050
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-06-29 23:37:35 +0100

    new commands and stuff

---
+++ cogs/misc_commands.py
@@ -0,0 +1,41 @@
+import discord
+import aiohttp
+import random
+
+from discord.ext import commands
+
+BASE_URL = "https://crunchy-bot.live/api"
+RANDOM_THUMBS = [
+    'https://cdn.discordapp.com/attachments/680350705038393344/717784208075915274/exitment.png',
+    'https://cdn.discordapp.com/attachments/680350705038393344/717784643117777006/wow.png',
+    'https://cdn.discordapp.com/attachments/680350705038393344/717784215986634953/cheeky.png',
+    'https://cdn.discordapp.com/attachments/680350705038393344/717784211771097179/thank_you.png'
+]
+
+
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="todayspicks", aliases=['picks', 'dailyanime', 'tp'])
+    async def anime_details(self, ctx):
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(f"{BASE_URL}/anime/daily") as resp:
+                if resp.status != 200:
+                    return await ctx.send("<:HimeSad:676087829557936149> Oh no! Something seems to have gone wrong,"
+                                          " please try again later.")
+                else:
+                    listed_items = await resp.json()
+
+        embed = discord.Embed(title="Today's Anime Picks!", color=self.bot.colour)
+        for i, anime in enumerate(listed_items):
+            url_safe = anime.get('title').replace(" ", "-").replace(".", "").replace(":", "")
+            url = "https://www.crunchyroll.com/{}".format(url_safe)
+            embed.add_field(name="\u200b", value=f"**{i + 1} ) - [{anime.get('title')}]({url})**", inline=False)
+        embed.set_image(url=random.choice(RANDOM_THUMBS))
+        embed.set_footer(text="Part of Crunchy, Powered by CF8")
+        return await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Misc(bot))
