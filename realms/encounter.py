434f1fdb442144d1862fd2ed1e96a5738074f0e0
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-07-07 15:43:10 +0100

    bits of stuff

--- realms/encounter.py
+++ realms/encounter.py
@@ -22,7 +22,7 @@ class Encounter:
         self.monster = None
         self.submit_callback = submit
 
-    def get_rand_monster(self):
+    def get_rand_monster(self) -> Monster:
         cr = randint(self.party.challenge_rating, self.party.challenge_rating + 5)
         monster = self.monster_manual.get_random_monster(cr)
         return monster
@@ -42,7 +42,7 @@ class Encounter:
         try:
             quest_no, _ = await self.bot.wait_for(
                 'quest_accept',
-                timeout=60,
+                timeout=120,
                 check=lambda no, user: user.id == self.ctx.author.id
             )
             self.monster = random_monsters[quest_no - 1]
@@ -53,20 +53,36 @@ class Encounter:
     async def battle(self):
         def check(msg: discord.Message):
             return (msg.author.id == self.ctx.author.id) and\
-                   (msg.channel.id == self.ctx.channel.id)
+                   (msg.channel.id == self.ctx.channel.id) and msg.content.startswith(self.ctx.prefix)
+
+        content = self.get_content(start=True)
+        await self.ctx.send(content)
+        valid = False
+        while not valid:
+            try:
+                message = await self.bot.wait_for('message', timeout=120, check=check)
+                command, *args = message.content.split(" ")
+                await self.process_commands_internally(command, args)
+
+            except asyncio.TimeoutError:
+                return await self.ctx.send("📛 This battle has expired! This is counted as failing to complete the quest.")
 
         battling = True
         while battling:
-            content = self.get_content(start=True)
+            content = self.get_content()
             await self.ctx.send(content)
             try:
                 message = await self.bot.wait_for('message', timeout=30, check=check)
+                print(message.content)
             except asyncio.TimeoutError:
                 battling = False
                 await self.ctx.send("📛 This battle has expired! This is counted as failing to complete the quest.")
 
-    def get_content(self, start=False):
+    def get_content(self, start=False) -> str:
         if start:
             return f"**Roll initiative! Do:** `{self.ctx.prefix}roll 1d20`"
         else:
-            pass
+            return "nom"
+
+    async def process_commands_internally(self, command: str, args: list):
+        p
