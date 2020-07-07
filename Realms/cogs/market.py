93148a8d9acf9d5d54dc73a2b9843ecee8eba39e
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-07-07 10:03:30 +0100

    finished up adding balance

--- Realms/cogs/market.py
+++ Realms/cogs/market.py
@@ -1,12 +1,33 @@
+import discord
+
 from discord.ext import commands
 
 from realms.datastores.database import MongoDatabase
 from realms.static import Database
+from realms.user_characters import UserCharacters
 
 
 class MarketCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(aliases=['balance', 'bal'])
+    async def pocket(self, ctx: commands.Context):
+        user_area = UserCharacters(ctx.author.id, Database.db)
+        embed = discord.Embed(color=self.bot.colour)
+        embed.set_author(name=f"{ctx.author.name}'s Adventures Bank", icon_url=ctx.author.avatar_url)
+        embed.description = f"\💠 **Platinum Pieces -** `{user_area.platinum}pp`\n" \
+                            f"\🔹 **Gold Pieces -** `{user_area.gold}gp`\n" \
+                            f"\🔸**Copper Pieces -** `{user_area.copper}cp`\n"
+        embed.set_footer(text="You can earn money by completing quests and voting.")
+        await ctx.send(embed=embed)
+
+    @commands.command(name="encounter")
+    async def encounter(self, ctx: commands.Context):
+        pass
+
+    
+
 
 
 def setup(bot):
