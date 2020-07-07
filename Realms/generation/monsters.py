b816170289074a85b38c45c63ac6d08d78d0148a
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-07-07 10:48:10 +0100

    encounters

--- Realms/generation/monsters.py
+++ Realms/generation/monsters.py
@@ -1,5 +1,11 @@
 import random
+import json
 
 
-async def get_random_monster():
-    pass
+class MonsterManual:
+    def __init__(self):
+        with open(r'realms/generation/monster_name.json', 'r') as file:
+            self.random_names = json.load(file)
+
+    async def get_random_monster(self, challenge_rating):
+        pass
